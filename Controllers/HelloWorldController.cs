a90ac48d671e09ecb6b9533516b3d5c0192fd2ba
Saqib Amin <saqibamin257@gmail.com>
2022-05-10 10:23:51 +0100

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+        //public string Welcome(string name,int numTimes = 1 ) 
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTime is: {numTimes}");
+        //}
+
+        public IActionResult Index() 
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["name"]=name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        }
+    }
+}
